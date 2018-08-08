a3f78b77def3f489979c16d763587948a99c3c3c
lixiaolong <hy1404381881@163.com>
2018-08-08 14:51:22 +0800

    java-concurrency-in-practice

---
+++ CodePractise/java-concurrency-in-practice/src/main/java/com/lizy/java_concurrency_in_practice/App.java
@@ -0,0 +1,13 @@
+package com.lizy.java_concurrency_in_practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
